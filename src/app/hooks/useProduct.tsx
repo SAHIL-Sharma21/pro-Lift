062e93d838838a6d2e720f3990e124975e41ca7a
Sahil <sahilsharma2000dtc@gmail.com>
2024-10-23 21:03:09 +0530

    use product hook completed

--- src/app/hooks/useProduct.tsx
+++ src/app/hooks/useProduct.tsx
@@ -1,10 +1,49 @@
+'use client';
 //use product hoooks
 import {useDispatch, useSelector} from 'react-redux'
 import {AppDispatch, RootState} from '@/app/lib/redux/store';
+import { useCallback } from 'react';
+import {fetchProducts, createProduct, deleteProduct, getProductById, updateProduct} from '@/app/lib/redux/slices/productSlice';
+import { ProductCreate, ProductUpdate } from '../types/products.types';
 
 
 export const useProduct = () => {
     const dispatch = useDispatch<AppDispatch>();
     const {products, loading, error,  selectedProduct, currentPage, totalPage} = useSelector((state: RootState) => state.product);
 
+    const getAllProducts = useCallback((page?: number, limit?: number) => {
+        return dispatch(fetchProducts({page, limit}));
+    }, [dispatch]);
+
+    const addProducts = useCallback((productData: ProductCreate) => {
+        return dispatch(createProduct(productData))
+    }, [dispatch]);
+
+    //may error can come
+    const editProduct = useCallback((productData: ProductUpdate, productId: string) => {
+        return dispatch(updateProduct({productId, payload: productData}));
+    }, [dispatch]);
+
+    const removeProduct = useCallback((productId: string) => {
+        return dispatch(deleteProduct(productId));
+    }, [dispatch]);
+
+
+    const fetchProductById = useCallback((productId: string) => {
+        return dispatch(getProductById(productId));
+    }, [dispatch]);
+
+    return {
+        products,
+        loading,
+        error,
+        getAllProducts,
+        addProducts,
+        editProduct,
+        removeProduct,
+        fetchProductById,
+        selectedProduct,
+        currentPage,
+        totalPage
+    };
 }
