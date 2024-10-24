9380704d62dd994b45526d427a44fa097dc03beb
Sahil <sahilsharma2000dtc@gmail.com>
2024-10-24 16:49:38 +0530

    statted building product list component

--- src/app/components/dashboard/ProductList.tsx
+++ src/app/components/dashboard/ProductList.tsx
@@ -1,10 +1,46 @@
 'use client'
 
-import React from 'react'
+import React, { use, useEffect } from 'react'
+import {useProduct} from '@/app/hooks/useProduct'
+import {Table, TableBody, TableHead, TableHeader, TableRow} from '@/components/ui/table'
 
 const ProductList = () => {
+
+  const {loading, error, getAllProducts, products} = useProduct();
+
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+
+
+  if(loading) return <div>Loading Products...</div>;
+  if(error) return <div>Error: {error}</div>;
+
+
   return (
-    <div>ProductList</div>
+    <>
+      <div className='space-y-4'>
+        <h2 className='text-2xl font-bold'>Products</h2>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Product Name</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   )
 }
 
