a12fda3e4875ead699702905d9d0e2a0d485dea9
Sahil <sahilsharma2000dtc@gmail.com>
2024-10-28 16:56:50 +0530

    dashboard order page implemneted

--- src/app/dashboard/(route)/orders/page.tsx
+++ src/app/dashboard/(route)/orders/page.tsx
@@ -1,17 +1,56 @@
 'use client'
 
-import { Table } from "@/components/ui/table"
+import { useOrder } from "@/app/hooks/useOrder"
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { useEffect } from "react";
 
 export default function OrdersPage() {
 
-    //making the useOder Hooks for using orders
+    const {loading, error, getOrders, orders} = useOrder();
+
+    useEffect(() => {
+        getOrders();
+    }, [getOrders]);
+
+    if(loading) return <div>Loading orders....</div>
+    if(error) return <div>Error: {error}</div>
 
     return (
         <div className="space-y-4">
             <h1 className="text-3xl font-bold">Orders</h1>
             <Table>
-
-
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>OrderId</TableHead>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>TotalAmount</TableHead>
+                        <TableHead>Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {/* length is not comming as undefined  have to debug here*/}
+                    {orders.length > 0 ? (
+                        <>
+                            {orders.map((order) => (
+                                <TableRow key={order.id}>
+                                    <TableCell>{order.id}</TableCell>
+                                    <TableCell>{new Date(order.createdAt).toLocaleDateString()}</TableCell>
+                                    <TableCell>{order.status}</TableCell>
+                                    <TableCell>Rs:{order.totalAmount}</TableCell>
+                                    <TableCell>
+                                        <Button variant="outline" size="sm">View Details</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </>
+                    ) : (
+                        <>
+                            <h1>No Orders are there</h1>
+                        </>
+                    )}
+                </TableBody>
             </Table>
         </div>
     )
