c0903fae20fee0564a851b6601c52198162d537a
Sahil <sahilsharma2000dtc@gmail.com>
2024-11-26 23:06:20 +0530

    feat: order summary and payment processor is implemented:

--- src/app/components/checkout/OrderSummary.tsx
+++ src/app/components/checkout/OrderSummary.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { useOrder } from '@/app/hooks/useOrder';
+import { Button } from '@/components/ui/button';
 import React from 'react'
 
 interface OrderSummaryProps {
@@ -8,8 +10,39 @@ interface OrderSummaryProps {
 }
 
 const OrderSummary = ({onNext, onPrev}: OrderSummaryProps) => {
+
+  const {currentOrder, addresses} = useOrder();
+
+  if(!currentOrder){
+    return <div>No Order found</div>
+  }
+
   return (
-    <div>OrderSummary</div>
+    <>
+      <div className='space-y-4'>
+        <h2 className='text-2xl font-bold'>Order Summary</h2>
+        <div>
+          <h3 className='font-semibold'>Items:</h3>
+          {currentOrder.items.map((item) => (
+            <div key={item.id} className='flex justify-between'>
+              <span>{item.productId}</span> {/* have to debug heree*/}
+              <span>Rs: {item.price * item.quantity}</span>
+            </div>
+          ))}
+        </div>
+        <div className='font-semibold'>
+          Total: Rs: {currentOrder.totalAmount}
+        </div>
+        <div>
+          <h3 className='font-semibold'>Shipping Address ID:</h3>
+          <p>{addresses[0].id}</p>
+        </div>
+        <div className='flex justify-between'>
+          <Button onClick={onPrev}>Back</Button>
+          <Button onClick={onNext}>Proceed to checkout</Button>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/app/components/checkout/PaymentProcessor.tsx
+++ src/app/components/checkout/PaymentProcessor.tsx
@@ -1,16 +1,75 @@
 'use client'
 
-import React from 'react'
+import { useOrder } from '@/app/hooks/useOrder';
+import React, { useEffect } from 'react'
 
 interface PaymentProcessorProps {
     onPaymentSuccess: () => void;
     onPaymentFailure: () => void;
 }
 
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+
 const PaymentProcessor = ({onPaymentSuccess, onPaymentFailure}: PaymentProcessorProps) => {
+
+  const {currentOrder, razorpayOrderId, verifyPaymentAction, addresses} = useOrder();
+
+  useEffect(() => {
+    if(razorpayOrderId){
+      const script = document.createElement('script');
+      script.src = `https://checkout.razorpay.com/v1/checkout.js`;
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        const options = {
+          key: process.env.NEXT_PUBLIC_RAZORPAY_KEY!,
+          amount: currentOrder?.totalAmount! * 100, // razorpay expects amount in paise
+          curreny: "INR",
+          name: "Pro Lift",
+          description: "Payment for order",
+          order_id: razorpayOrderId,
+          handler: function(response: any) {
+            verifyPaymentAction({
+              razorpayOrderId: response.razorpay_order_id,
+              razorpayPaymentId: response.razorpay_payment_id,
+              razorpaySignature: response.razorpay_signature,
+              addressId: currentOrder?.id! // TODO:here to debug
+            });
+            onPaymentSuccess();
+          },
+          prefill:{
+            name: "customer name",
+            email: "customer@email.com",
+            contact: "9999999999"
+          },
+          theme: {
+            color: "#F3723A"
+          }
+        };
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+      };
+      
+      return () => {
+        document.body.removeChild(script);
+      }
+    }
+  }, [razorpayOrderId, currentOrder, verifyPaymentAction, onPaymentSuccess]);
+
   return (
-    <div>PaymentProcessor</div>
+   <>
+    <div className='text-center'>
+      <h2 className='text-2xl font-bold mb-4'>Processing Payment</h2>
+      <p>Please complete the payment in razorpay window.</p>
+    </div>
+   </>
   )
 }
 
-export default PaymentProcessor
+export default PaymentProcessor;
